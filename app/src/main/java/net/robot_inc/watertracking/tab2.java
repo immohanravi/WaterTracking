a7e5607ef9e9637512f0331ea4739feda8886698
robotinc <robotinc.in@gmail.com>
2017-02-13 00:10:39 +0530

    testing tab2

--- app/src/main/java/net/robot_inc/watertracking/tab2.java
+++ app/src/main/java/net/robot_inc/watertracking/tab2.java
@@ -1,20 +1,32 @@
 package net.robot_inc.watertracking;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import net.robot_inc.watertracking.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class tab2 extends Fragment {
 
-
+    Button loadImage;
+    ImageView iv;
+    private static int RESULT_LOAD_IMAGE = 1;
     public tab2() {
         // Required empty public constructor
     }
@@ -24,7 +36,45 @@ public class tab2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab2,container,false);
+        iv = (ImageView) view.findViewById(R.id.imageView2);
+        loadImage = (Button) view.findViewById(R.id.button2);
+        loadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+        return view;
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            iv.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
     }
 
 }
