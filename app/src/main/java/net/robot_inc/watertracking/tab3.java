fb57d1a3cba8b52ba0286785168965d8a2978615
Mohan <immohanravi@gmail.com>
2017-02-18 20:11:23 +0530

    in linux system

--- app/src/main/java/net/robot_inc/watertracking/tab3.java
+++ app/src/main/java/net/robot_inc/watertracking/tab3.java
@@ -1,19 +1,31 @@
 package net.robot_inc.watertracking;
 
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
 import net.robot_inc.watertracking.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class tab3 extends Fragment {
+public class tab3 extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    //GoogleApiClient mGoogleApiClient;
 
     public tab3() {
         // Required empty public constructor
@@ -24,7 +36,60 @@ public class tab3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+/*
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();*/
         return inflater.inflate(R.layout.fragment_tab3, container, false);
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        /*
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(getActivity(), 1);
+            } catch (IntentSender.SendIntentException e) {
+                // Unable to resolve, message user appropriately
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), getActivity(), 0).show();
+        }*/
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        /*
+        if (mGoogleApiClient.isConnected()==false){
+            mGoogleApiClient.connect();
+        }*/
+
+    }
+    @Override
+    public  void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        /*
+        switch (requestCode) {
+
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }*/
+    }
+
 
 }
